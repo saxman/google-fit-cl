656b58f4ff07c6a688eeea86139c451d040f51be
Paul R. Saxman <saxman@google.com>
2016-08-16 15:17:19 -0700

    General clean-up and addition of nutrition samples

--- VARS.sh
+++ VARS.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
-TZ_OFFSET_SECS=$(echo '60 * 60 * 7' | bc)
+ENDPOINT=https://www.googleapis.com/fitness/v1/users/me
+
+## All time calculations here in seconds
+
+SECS_IN_DAY=`expr 60 \* 60 \* 24`
+MS_IN_DAY=`expr $SECS_IN_DAY \* 1000`
+TZ_OFFSET=`expr 60 \* 60 \* 7`
 
 NOW=`date +%s`
-START_OF_DAY=$(echo "$NOW - ($NOW % 86400) + $TZ_OFFSET_SECS" | bc)
+START_OF_TODAY=$(echo "$NOW - ($NOW % $SECS_IN_DAY) + $TZ_OFFSET" | bc)
 
-ENDPOINT=https://www.googleapis.com/fitness/v1/users/me
+if [ $START_OF_TODAY -gt $NOW ] ; then
+   START_OF_TODAY=`expr $START_OF_TODAY - $SECS_IN_DAY`
+fi
+
+START_OF_YESTERDAY=`expr $START_OF_TODAY - $SECS_IN_DAY`
+
+NOW_STR=`date -jr $NOW`
+NOW_MS=`expr $NOW \* 1000`
+
+START_OF_TODAY_STR=`date -jr $START_OF_TODAY`
+START_OF_TODAY_MS=`expr $START_OF_TODAY \* 1000`
+
+START_OF_YESTERDAY_STR=`date -jr $START_OF_YESTERDAY`
+START_OF_YESTERDAY_MS=`expr $START_OF_YESTERDAY \* 1000`
+
+## If script run instead of sourced, output debug info
+if [ $(basename $0) = 'VARS.sh' ]; then
+   echo NOW: $NOW_STR
+   echo START_OF_TODAY: $START_OF_TODAY_STR
+   echo START_OF_YESTERDAY: $START_OF_YESTERDAY_STR
+fi
 
--- calories_total_today.sh
+++ calories_total_today.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 source ACCESS_TOKEN.sh
 source VARS.sh
 
-DATASET_1=derived:com.google.calories.expended:com.google.android.gms:from_bmr
-DATASET_2=derived:com.google.calories.expended:com.google.android.gms:from_activities
-DATASET_3=derived:com.google.calories.expended:com.google.android.gms:default_calories_expended
-DATASET_4=derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended
-DATASET_5=derived:com.google.calories.expended:com.google.android.gms:platform_calories_expended
-DATASET_6=derived:com.google.calories.bmr:com.google.android.gms:merged
+DATASET=dataset:aggregate
+DATATYPE=com.google.calories.expended
 
-DATASET=$DATASET_1
+json="{
+  \"aggregateBy\": [{
+    \"dataTypeName\":
+      \"$DATATYPE\"
+  }],
+  \"bucketByTime\": { \"durationMillis\": ${MS_IN_DAY} },
+  \"startTimeMillis\": ${START_OF_TODAY_MS},
+  \"endTimeMillis\": ${NOW_MS}
+}"
 
-url=$ENDPOINT/dataSources/$DATASET/datasets/${START_OF_DAY}000000000-${NOW}000000000
-echo $url; echo
+url=$ENDPOINT/$DATASET
+
+echo $url; echo; echo $json; echo
 
 curl \
+  -H "Content-Type: application/json" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -X POST \
+  --data "$json" \
   $url
 
